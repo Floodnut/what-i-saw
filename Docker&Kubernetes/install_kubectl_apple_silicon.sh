33c8d406dc27aef7a50d74364920a2932f8bf953
Floodnut <gsniper777@gmail.com>
2023-11-15 22:53:54 +0900

    feat: minikube install script

---
+++ Docker&Kubernetes/install_kubectl_apple_silicon.sh
@@ -0,0 +1,5 @@
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/arm64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/arm64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | shasum -a 256 --check
+
+kubectl version --client --output=yaml    
---
+++ Docker&Kubernetes/install_minikube_apple_silicon.sh
@@ -0,0 +1,2 @@
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-arm64
+sudo install minikube-darwin-arm64 /usr/local/bin/minikube
