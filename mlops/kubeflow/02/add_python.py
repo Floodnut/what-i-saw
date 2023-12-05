45b137a13f96cfcb5e8d15b15a513ba409357316
Floodnut <gsniper777@gmail.com>
2023-12-06 00:25:28 +0900

    refactor: add_python

--- mlops/kubeflow/02/add_python.py
+++ mlops/kubeflow/02/add_python.py
@@ -3,40 +3,16 @@
 import kfp
 from kfp import components
 from kfp import dsl
-import requests
+from kubeflow_host import KubeflowHost
 from dotenv import dotenv_values
 
 EXPERIMENT_NAME = 'Add number pipeline'
 BASE_IMAGE = "python:3.7"
 
-class KubeflowHost():
-    def __init__(self, host, username, password, namespace):
-        self.host = host
-        self.kubeflow_host = f"{host}/pipeline"
-        self.namespace = namespace
-        
-        self.data = {
-            "login": username,
-            "password": password,
-        }
-        
-    def get_session_cookie(self):
-        session = requests.Session()
-        response = session.get(self.host)
-
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded",
-        }
-                
-        session.post(response.url, headers=headers, data=self.data)
-        session_cookie = session.cookies.get_dict()["authservice_session"]
-        
-        return session_cookie
-
 
 @dsl.python_component(
     name='add_op',
-    description='adds two numbers',
+    description='adds two numbers', 
     base_image=BASE_IMAGE
 )
 def add(a: float, b: float) -> float:
@@ -75,13 +51,10 @@ if __name__ == "__main__":
     )
     
     session_cookie = kubeflow_host.get_session_cookie()
+    kfp_client = kubeflow_host.get_kfp_client()
     
     arguments = {'a': '7', 'b': '8'}
-    kfp.Client(
-        host=kubeflow_host.kubeflow_host,
-        namespace=f"{kubeflow_host.namespace}",
-        cookies=f"authservice_session={session_cookie}",
-    ).create_run_from_pipeline_func(
+    kfp_client.create_run_from_pipeline_func(
         calc_pipeline,
         arguments=arguments,
         experiment_name=EXPERIMENT_NAME
